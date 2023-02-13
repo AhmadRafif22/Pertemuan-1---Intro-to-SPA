8e18f5a7cab59f3e13c07c210cb4364a9fdfb5eb
AhmadRafif22 <rafifayuzaki123@gmail.com>
2023-02-13 09:46:54 +0700

    add input field at homeScreen use DOM manipulation

--- index.js
+++ index.js
@@ -5,23 +5,55 @@ const root = document.getElementById("root");
 // mencegah ngelink, ubah url, dan mengubah konten root
 linkHome.onclick = function(event) {
     event.preventDefault();
-    root.textContent = "Welcome to Home";
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
     // mengubah url
     history.pushState(null, "", event.target.href);
 };
 
 linkAbout.onclick = function(event) {
     event.preventDefault();
-    root.textContent = "Welcome to About";
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
     // mengubah url
     history.pushState(null, "", event.target.href);
 };
 
-// percabangan ketika mengakses url langsung
-if (location.hash === "#about") {
-    root.textContent = "Welcome to About";
-} else if (location.hash === "#home") {
-    root.textContent = "Welcome to Home";
+// Function untuk rendering client side halaman about
+function AboutScreen() {
+    const text = document.createElement("p");
+    text.textContent = "Welcome to About";
+
+    return text;
 }
 
+// Function untuk rendering client side halaman home
+function HomeScreen() {
+    const textPreview = document.createElement("p");
+
+    const input = document.createElement("input");
+    input.oninput = function (event) {
+        textPreview.textContent = event.target.value;
+    };
+    input.placeholder = "Enter your name";
+
+    const div = document.createElement("div");
+    div.append(input);
+    div.append(textPreview);
+    
+    return div;
+}
+
+// percabangan ketika mengakses url langsung
+if (location.hash === "#about") {
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
+} else if (location.hash === "#home") {
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
+}
 
